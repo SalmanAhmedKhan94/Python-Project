bc1cc0ae59d63b6188d7a55fb759d4b41b344e42
SalmanAhmedKhan94 <123613801+SalmanAhmedKhan94@users.noreply.github.com>
2023-01-26 09:13:15 +0530

    Adding Type Casting

---
+++ Type casting.py
@@ -0,0 +1,35 @@
+#taking a float variable
+variableFloat=123.789
+print(type(variableFloat))
+#coverting float into integer
+convertingFloatIntoInt=int(variableFloat)
+print(convertingFloatIntoInt)
+#checking type converted into integer
+print(type(convertingFloatIntoInt))
+
+#taking complex variable
+complexVariable=10+5j
+print(type(complexVariable))
+#converting into integer
+#convertingComplexIntoInteger=int(complexVariable)
+#print(convertingComplexIntoInteger, type(convertingComplexIntoInteger))
+
+#boolen
+print(int(True))
+print(int(False))
+# float and string
+print(int("100"))
+
+
+#float
+print(float("20.5"))
+
+#taking variable and input value into new variable
+variableA="10.5"
+floatX = float(variableA)
+print(int(floatX))
+
+#complex
+
+print(complex(10))
+
