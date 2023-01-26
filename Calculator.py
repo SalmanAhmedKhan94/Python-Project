9c6b1517b7b8a49b4b6f2849246d52af0c03aa6f
SalmanAhmedKhan94 <123613801+SalmanAhmedKhan94@users.noreply.github.com>
2023-01-26 09:19:26 +0530

    Adding Calculator

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+print("'Enter the operation you want to per form' 'addtion = +' 'substraction = -' 'multiplication = *' 'division = /'\n")
+input_operation = input(operation)
+print("Enter the 1st digit\n")
+input_1st = input()
+print("Enter the 2nd digit\n")
+input_2nd = input()
+print("Your Answer is ="(input_1st = input(),input_operation = input(),input_2nd = input))
